e0789c4c8612dbd933087025dd391d6228f13c79
Rasmus Nurro <82094563+RasmusNurroKoulu@users.noreply.github.com>
2022-03-01 11:35:06 +0200

    Add files via upload

---
+++ palvelut.py
@@ -0,0 +1,24 @@
+class Asiakas():
+    def __init__(self):
+        self._nimi = nimi
+        self._asiakasnro = 1
+        self._ika = ika
+    def _luo_nro():
+        pass
+
+class Palvelu(Asiakas):
+    def __init__(self):
+        self.tuotenimi = tuotenimi
+        self._asiakkaat = Asiakas()
+
+    def __luo_asiakasrivi(Asiakas):
+        pass
+
+    def lisaa_asiakas(Asiakas):
+        pass
+
+    def tulosta_asiakkaat():
+        pass
+class ParempiPalvelu(Palvelu):
+    def __init__(self):
+        self._edut = [str]
