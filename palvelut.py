57473e8a02a7cf10f35ed44fe002961f9356ad7f
Rasmus Nurro <82094563+RasmusNurroKoulu@users.noreply.github.com>
2022-03-08 10:06:33 +0200

    Update palvelut.py

--- palvelut.py
+++ palvelut.py
@@ -1,5 +1,18 @@
 class Asiakas():
+    """Luokka, joka antaa asiakkaalle nimen, numeron, iän ja luo numeron
+    
+    Julkiset methodit:
+    """
     def __init__(self):
+        """Luo asiakkaan tiedot.
+
+        :param nimi: antaa nimen
+        :type nimi: str
+        :param _asiakasnro: antaa asiakas numeron
+        :type _asiakasnro: int
+        :param _ika: antaa iän
+        :type _ika: int
+        """
         self._nimi = nimi
         self._asiakasnro = 1
         self._ika = ika
@@ -7,18 +20,34 @@ class Asiakas():
         pass
 
 class Palvelu(Asiakas):
+    """Luokka, millä voit muokata asiakkaita 
+    
+    Julkiset methodit:
+        lisaa_asiakas(_asiakkaat)
+        tulosta_asiakkaat()
+    """
     def __init__(self):
+        """Luo tuotenimen ja hakee asiakkaat.
+
+        :param tuotenimi: antaa tuotenimen
+        :type tuotenimi: str
+        :param _asiakasnro: antaa asiakas numeron
+        :type _asiakasnro: int
+        :param _asiakkaat: hakee asiakkaat
+        :type _asiakkaat: Union[int, float]
+        """
         self.tuotenimi = tuotenimi
         self._asiakkaat = Asiakas()
 
-    def __luo_asiakasrivi(Asiakas):
+    def __luo_asiakasrivi(_asiakkaat):
         pass
 
-    def lisaa_asiakas(Asiakas):
+    def lisaa_asiakas(_asiakkaat):
         pass
 
     def tulosta_asiakkaat():
         pass
+
 class ParempiPalvelu(Palvelu):
     def __init__(self):
         self._edut = [str]
