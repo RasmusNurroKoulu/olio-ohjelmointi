26ff788c5baf226dedd69c49f9c92d2c96162071
Rasmus Nurro <82094563+RasmusNurroKoulu@users.noreply.github.com>
2022-03-08 11:09:11 +0200

    Update palvelut.py

--- palvelut.py
+++ palvelut.py
@@ -1,9 +1,9 @@
-class Asiakas():
+class Asiakas:
     """Luokka, joka antaa asiakkaalle nimen, numeron, iän ja luo numeron
     
     Julkiset methodit:
     """
-    def __init__(self):
+    def __init__(self, nimi, ika):
         """Luo asiakkaan tiedot.
 
         :param nimi: antaa nimen
@@ -14,8 +14,28 @@ class Asiakas():
         :type _ika: int
         """
         self._nimi = nimi
-        self._asiakasnro = 1
         self._ika = ika
+        self._asiakasnro = 2
+
+    def get_nimi(self):
+        try: 
+            self._nimi
+        except NameError:
+            print("Suosittelen antamaan uuden nimen")
+
+    def set_nimi(self, uusi_ika):
+        self._nimi = uusi_ika
+
+    def get_ika(self):
+        try:
+            return self._ika
+        except ValueError:
+            print("Kehotan antamaan uuden nimen ja iän")
+
+    def set_ika(self, ika):
+        self._ika = ika
+
+
     def _luo_nro():
         pass
 
@@ -26,7 +46,7 @@ class Palvelu(Asiakas):
         lisaa_asiakas(_asiakkaat)
         tulosta_asiakkaat()
     """
-    def __init__(self):
+    def __init__(self, tuotenimi):
         """Luo tuotenimen ja hakee asiakkaat.
 
         :param tuotenimi: antaa tuotenimen
@@ -56,8 +76,13 @@ class ParempiPalvelu(Palvelu):
         poista_etu(),
         tulosta_edut()
     """
-    def __init__(self):
-        self._edut = [str]
+    def __init__(self, tuotenimi):
+        """Ottaa edut tuotteesta
+
+        :param _edut: asettaa edut tuotenimi listaksi
+        :type _edut: str
+        """
+        self._edut = [tuotenimi]
     
     def lisaa_etu(edut):
         pass
